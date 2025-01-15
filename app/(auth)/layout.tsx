a57ad6e55d4644d9fc3c0b3e84b751fa151024b2
Dean <deanadrian644@gmail.com>
2025-01-15 14:36:22 +0700

    login page responsiveness

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,18 +6,18 @@ export default function AuthLayout({
   children: React.ReactNode 
 }) {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-[#F2F3F4] p-6 md:p-10">
-      <div className="w-full h-full">
-        <Card className="h-full bg-white shadow-md px-20 overflow-hidden">
-          <div className="flex flex-col md:flex-row items-center justify-start gap-4 md:gap-8">
-            <img src="/images/logo.svg" className="w-40 h-40" />
-            <h1 className="text-3xl font-extrabold font-secular text-center md:text-start">Pengelolaan Dana Pendidikan Sekolah Zaidan Educare</h1>
-          </div>
-          <div className="h-full">
-            {children}
-          </div>
-        </Card>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#F2F3F4] md:px-8 lg:px-20 xl:px-40 2xl:px-96">
+      <Card className="bg-white w-full shadow-md px-4 md:px-10 pb-12">
+        <div className="flex flex-col lg:flex-row items-center justify-start lg:gap-8">
+          <img src="/images/logo.svg" className="w-36 h-36 md:w-40 md:h-40" />
+          <h1 className="text-xl md:text-2xl xl:text-3xl font-extrabold font-secular text-center lg:text-start">
+            Pengelolaan Dana Pendidikan Sekolah Zaidan Educare
+          </h1>
+        </div>
+        <div>
+          {children}
+        </div>
+      </Card>
     </div>
   )
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -55,94 +55,90 @@ export default function LoginPage() {
   }
 
   return (
-    <div className={cn("flex flex-col gap-6 h-full")}>
-      <div className="flex justify-center xl:grid xl:grid-cols-2 h-full xl:mb-60 xl:mx-40">
-        <div className="relative hidden xl:block">
-          <img
-            src="/images/auth-logo.png"
-            alt="Image"
-            className="absolute inset-0 h-full w-full dark:brightness-[0.2] dark:grayscale object-scale-down "
-          />
+    <div className="flex justify-center items-center gap-6">
+      <img
+        src="/images/auth-logo.png"
+        alt="Image"
+        className="lg:block hidden inset-0 object-scale-down"
+      />
+      <div className="w-full">
+        <div className="flex flex-col justify-center items-center gap-4 mt-8 lg:mt-0">
+          <h1 className="font-secular font-bold text-xl xl:text-2xl text-center">Selamat Datang</h1>
+          <p className="font-secular text-md xl:text-lg text-center">Silahkan masuk ke dalam sistem</p>
         </div>
-        <div className="w-full px-20 xl:px-0">
-          <div className="flex flex-col justify-center items-center gap-4 p-8">
-            <h1 className="font-secular font-bold text-4xl text-center md:text-start">Selamat Datang</h1>
-            <p className="font-secular text-lg text-center md:text-start">Silahkan masuk ke dalam sistem</p>
-          </div>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-10 px-0 xl:px-10 ">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-secular font-bold text-xl">
-                      Username <span className="text-red-500 text-sm">*</span>
-                    </FormLabel>
-                    <FormControl>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-10 pt-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-secular font-bold text-md xl:text-lg">
+                    Username <span className="text-red-500 text-sm">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input 
+                      placeholder="admin"
+                      {...field}
+                      className="bg-[#CBDCEB] focus-visible:ring-[#CBDCEB]/50 h-14"
+                    />
+                  </FormControl>
+                  <FormMessage className="text-sm" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-secular font-bold text-md xl:text-lg">
+                    Password <span className="text-red-500 text-sm">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
                       <Input 
-                        placeholder="admin"
-                        {...field}
+                        placeholder="*****" {...field} 
+                        type={showPassword ? "text" : "password"}
                         className="bg-[#CBDCEB] focus-visible:ring-[#CBDCEB]/50 h-14"
                       />
-                    </FormControl>
-                    <FormMessage className="text-sm" />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-secular font-bold text-xl">
-                      Password <span className="text-red-500 text-sm">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <div className="relative">
-                        <Input 
-                          placeholder="*****" {...field} 
-                          type={showPassword ? "text" : "password"}
-                          className="bg-[#CBDCEB] focus-visible:ring-[#CBDCEB]/50 h-14"
-                        />
-                        <TooltipProvider>
-                          <Tooltip>
-                            <TooltipTrigger asChild>
-                              <Button 
-                                variant="ghost" 
-                                size="icon" 
-                                className="absolute right-0 top-1/2 transform -translate-y-1/2 p-0 h-14 w-14 hover:bg-transparent"
-                                onClick={() => setShowPassword((prev) => !prev)}
-                                type="button"
-                              >
-                                {showPassword ? 
-                                  <EyeOff /> : 
-                                  <Eye />
-                                }
-                              </Button>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                              <p>
-                                {showPassword ? "Sembunyikan" : "Tampilkan"} password
-                              </p>
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      </div>
-                    </FormControl>
-                    <FormMessage className="text-sm" />
-                  </FormItem>
-                )}
-              />
-              <Button 
-                type="submit"
-                className="w-full h-14 bg-[#608BC1] hover:bg-[#5274A2] duration-200 text-white"
-              >
-                Login
-              </Button>
-            </form>
-          </Form>
-        </div>
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <Button 
+                              variant="ghost" 
+                              size="icon" 
+                              className="absolute right-0 top-1/2 transform -translate-y-1/2 p-0 h-14 w-14 hover:bg-transparent"
+                              onClick={() => setShowPassword((prev) => !prev)}
+                              type="button"
+                            >
+                              {showPassword ? 
+                                <EyeOff /> : 
+                                <Eye />
+                              }
+                            </Button>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>
+                              {showPassword ? "Sembunyikan" : "Tampilkan"} password
+                            </p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </div>
+                  </FormControl>
+                  <FormMessage className="text-sm" />
+                </FormItem>
+              )}
+            />
+            <Button 
+              type="submit"
+              className="w-full h-14 bg-[#608BC1] hover:bg-[#5274A2] duration-200 text-white"
+            >
+              Login
+            </Button>
+          </form>
+        </Form>
       </div>
     </div>
   )
