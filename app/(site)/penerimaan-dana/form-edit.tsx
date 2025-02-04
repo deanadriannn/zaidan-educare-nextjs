9fd260184a9afe956e7f80c166ac0105d55aba7e
Dean <deanadrian644@gmail.com>
2025-02-05 00:15:56 +0700

    penerimaan dana edit page

--- app/(site)/penerimaan-dana/form-edit.tsx
+++ app/(site)/penerimaan-dana/form-edit.tsx
@@ -78,15 +78,15 @@ const namaBankContoh = [
 
 const penerimaanDanaSchema = z.object({
   nominal: z.number({
-    required_error: "Nominal harus diisi",
+    required_error: "Nominal wajib diisi",
     invalid_type_error: "Nominal harus berupa angka",
   })
   .positive("Nominal tidak boleh negatif atau nol"),
   metodePembayaran: z
   .string({
-    required_error: "Metode pembayaran harus diisi",
+    required_error: "Metode pembayaran wajib diisi",
   })
-  .min(1, "Metode pembayaran harus diisi"),
+  .min(1, "Metode pembayaran wajib diisi"),
   namaBank: z.string().optional(), 
 })
 
@@ -97,8 +97,9 @@ export default function PenerimaanDanaForm() {
   const form = useForm<tagihanSiswaFormValues>({
     resolver: zodResolver(penerimaanDanaSchema),
     defaultValues: {
-      metodePembayaran: "",
-      namaBank: ""
+      nominal: 1000000,
+      metodePembayaran: "transfer",
+      namaBank: "bri"
     },
   })
 
@@ -128,7 +129,6 @@ export default function PenerimaanDanaForm() {
           <Label className="text-lg font-semibold text-muted-foreground">Nama</Label>
           <Label className="text-lg font-semibold text-muted-foreground">Kelas</Label>
           <Label className="text-lg font-semibold text-muted-foreground">NIS</Label>
-          <Label className="text-lg font-semibold text-muted-foreground">Tanggal Transaksi</Label>
         </div>
         <div className="flex flex-col gap-4">
           <p className="text-lg text-black">
@@ -140,149 +140,146 @@ export default function PenerimaanDanaForm() {
           <p className="text-lg text-black">
             1234445
           </p>
+        </div>
+        <div className="flex gap-6 ml-10">
+          <Label className="text-lg font-semibold text-muted-foreground">Tanggal Transaksi</Label>
           <p className="text-lg text-black">
             9 September 2021
           </p>
         </div>
       </div>
 
-      <div className="mt-8 flex flex-col gap-4 font-spartan mb-2">
-        <h1 className="font-bold text-black text-xl">Daftar Transaksi Pembayaran</h1>
-
-        <div className="flex gap-8">
-          <div className="flex flex-col gap-4">
-            <Label className="text-lg font-semibold text-muted-foreground">Jenis Pembayaran</Label>
-            <Label className="text-lg font-semibold text-muted-foreground">Waktu Pembayaran</Label>
-            <Label className="text-lg font-semibold text-muted-foreground">Status Cicilan</Label>
-          </div>
-          <div className="flex flex-col gap-4">
-            <p className="text-lg text-black">
-              SPP Januari
-            </p>
-            <p className="text-lg text-black">
-              1x
-            </p>
-            <p className="text-lg text-black">
-              Ya
-            </p>
-          </div>
-        </div>
-      </div>
-
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 font-spartan">
-          <FormField
-            control={form.control}
-            name="nominal"
-            render={({ field }) => {
-              const { error } = useFormField()
-              return (
-                <FormItem className="flex gap-4 items-center">
-                  <FormLabel 
-                    className={cn(
-                      error ? "text-destructive": "text-muted-foreground",
-                      "text-lg font-semibold"
-                    )}
-                  >
-                    Nominal
-                  </FormLabel>
-                  <div>
-                    <FormControl>
-                      <Input 
-                        type="number" 
-                        {...field}
-                        value={field.value || ""} // Untuk mencegah warning controlled/uncontrolled
-                        onChange={(e) => field.onChange(e.target.valueAsNumber)}
-                        className="w-full"
-                        disabled={isLoading}
-                      />
-                    </FormControl>
-                  </div>
-                </FormItem>
-              )
-            }}
-          />
+          <div className="mt-8 flex flex-col gap-4 font-spartan mb-2">
+            <h1 className="font-bold text-black text-xl">Daftar Transaksi Pembayaran</h1>
 
-          <FormField
-            control={form.control}
-            name="metodePembayaran"
-            render={({ field }) => {
-              const { error } = useFormField()
-              return (
-                <FormItem className="flex gap-4 items-center">
-                  <FormLabel 
-                    className={cn(
-                      error ? "text-destructive": "text-muted-foreground",
-                      "text-lg font-semibold"
-                    )}
-                  >
-                    Metode Pembayaran
-                  </FormLabel>
-                  <div>
-                    <Select onValueChange={field.onChange} defaultValue={field.value} disabled={isLoading}>
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Pilih Salah Satu" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        {metodePembayaranContoh.map((option) => (
-                          <SelectItem
-                            key={option.value}
-                            value={option.value}
-                          >
-                            {option.label}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                  </div>
-                </FormItem>
-              )
-            }}
-          />
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4">
+              <Label className="text-lg font-semibold text-muted-foreground col-span-1">Jenis Pembayaran</Label>
+              <p className="text-lg text-black col-span-1">
+                SPP Januari
+              </p>
+              <div className="md:col-span-2 lg:col-span-4 xl:col-span-6"></div>
 
-          {form.watch("metodePembayaran") === "transfer" && (
-            <FormField
-              control={form.control}
-              name="namaBank"
-              render={({ field }) => {
-                const { error } = useFormField()
-                return (
-                  <FormItem className="flex gap-4 items-center">
-                    <FormLabel 
-                      className={cn(
-                        error ? "text-destructive": "text-muted-foreground",
-                        "text-lg font-semibold"
-                      )}
-                    >
-                      Nama Bank
-                    </FormLabel>
-                    <div>
-                      <Select onValueChange={field.onChange} defaultValue={field.value} disabled={isLoading}>
+              <Label className="text-lg font-semibold text-muted-foreground">Waktu Pembayaran</Label>
+              <p className="text-lg text-black">
+                1x
+              </p>
+              <div className="md:col-span-2 lg:col-span-4 xl:col-span-6"></div>
+
+              <Label className="text-lg font-semibold text-muted-foreground">Status Cicilan</Label>
+              <p className="text-lg text-black">
+                Ya
+              </p>
+              <div className="md:col-span-2 lg:col-span-4 xl:col-span-6"></div>
+
+              <Label className={cn(
+                form.getFieldState("nominal").error ? "text-destructive": "text-muted-foreground",
+                "text-lg font-semibold col-span-1"
+              )}>Nominal</Label>
+              <FormField
+                control={form.control}
+                name="nominal"
+                render={({ field }) => {
+                  const { error } = useFormField()
+                  return (
+                    <FormItem className="flex gap-4 items-center col-span-1">
+                      <div>
                         <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Pilih Salah Satu" />
-                          </SelectTrigger>
+                          <Input 
+                            type="number" 
+                            {...field}
+                            value={field.value || ""} // Untuk mencegah warning controlled/uncontrolled
+                            onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                            className="w-full"
+                            disabled={isLoading}
+                          />
                         </FormControl>
-                        <SelectContent>
-                          {namaBankContoh.map((option) => (
-                            <SelectItem
-                              key={option.value}
-                              value={option.value}
-                            >
-                              {option.label}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </div>
-                  </FormItem>
-                )
-              }}
-            />
-          )}
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )
+                }}
+              />
+              <div className="md:col-span-2 lg:col-span-4 xl:col-span-6"></div>
+
+              <Label className={cn(
+                form.getFieldState("metodePembayaran").error ? "text-destructive": "text-muted-foreground",
+                "text-lg font-semibold col-span-1"
+              )}>Metode Pembayaran</Label>
+              <FormField
+                control={form.control}
+                name="metodePembayaran"
+                render={({ field }) => {
+                  const { error } = useFormField()
+                  return (
+                    <FormItem className="flex gap-4 items-center col-span-1">
+                      <div className="w-full">
+                        <Select onValueChange={field.onChange} defaultValue={field.value} disabled={isLoading}>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Pilih Salah Satu" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {metodePembayaranContoh.map((option) => (
+                              <SelectItem
+                                key={option.value}
+                                value={option.value}
+                              >
+                                {option.label}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )
+                }}
+              />
+              <div className="md:col-span-2 lg:col-span-4 xl:col-span-6"></div>
+
+              {form.watch("metodePembayaran") === "transfer" && (
+                <>
+                  <Label className={cn(
+                    form.getFieldState("namaBank").error ? "text-destructive": "text-muted-foreground",
+                    "text-lg font-semibold col-span-1"
+                  )}>Nama Bank</Label>
+                  <FormField
+                    control={form.control}
+                    name="namaBank"
+                    render={({ field }) => {
+                      return (
+                        <FormItem className="flex gap-4 items-center col-span-1">
+                          <div className="w-full">
+                            <Select onValueChange={field.onChange} defaultValue={field.value} disabled={isLoading}>
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder="Pilih Salah Satu" />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                {namaBankContoh.map((option) => (
+                                  <SelectItem
+                                    key={option.value}
+                                    value={option.value}
+                                  >
+                                    {option.label}
+                                  </SelectItem>
+                                ))}
+                              </SelectContent>
+                            </Select>
+                            <FormMessage />
+                          </div>
+                        </FormItem>
+                      )
+                    }}
+                  />
+                </>
+              )}
+            </div>
+          </div>
 
           {/* BUTTON SUBMIT */}
           <div className="flex justify-end gap-4">
